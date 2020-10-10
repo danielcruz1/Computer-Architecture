4b2698f3149fca25313f9d940ee61510110e2e74
Daniel  Cruz <danielcruz489@gmail.com>
2020-10-09 19:08:57 -0600

    mvp day 4

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -14,8 +14,21 @@ class CPU:
         self.pc = 0
         self.register = [0] * 8
         self.running = True
-        self.reg = [0] * 8
+        # self.reg = [0] * 8
         self.SP = 0xf4
+
+        #STEP 9: Branch Table implementation
+        self.branch_table = {
+            0b10000010: self.LDI,
+            0b01000111: self.PRN,
+            0b00000001: self.HLT,
+            0b10100000: self.ADD,
+            0b10100010: self.MUL,
+            0b01000101: self.PUSH,
+            0b01000110: self.POP,
+            0b00010001: self.RET,
+            0b01010000: self.CALL
+        }
 
     def load(self, filename):
         """Load a program into memory."""
@@ -23,14 +36,18 @@ class CPU:
         filename = sys.argv[1]
         address = 0
         with open(filename) as f:
-            for address, line in enumerate(f):
-                line = line.split('#')
+            #for address, line in enumerate(f):
+                #line = line.split('#')
+            for line in f:
+                line = line.split('#')[0].strip()
 
                 try:
-                    v = int(line[0], 2)
+                    #v = int(line[0], 2)
+                    v = int(line, 2)
                 except ValueError:
                     continue
                 self.ram[address] = v
+                #print([address], v)
                 address += 1
 
     def alu(self, op, reg_a, reg_b):
@@ -63,7 +80,7 @@ class CPU:
         ), end='')
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.register[i], end='')
 
         print()
 
@@ -74,21 +91,68 @@ class CPU:
     def ram_write(self, value, address):
         self.ram[address] = value
 
+    def HLT(self):
+        self.running = False
+
+    def LDI(self):
+        address = self.ram_read(self.pc + 1)
+        value = self.ram_read(self.pc + 2)
+        self.register[address] = value
+
+    def PRN(self):
+        address = self.ram_read(self.pc + 1)
+        value = self.register[address]
+        print(value)
+
+    def ADD(self):
+        operandA = self.ram_read(self.pc + 1)
+        operandB = self.ram_read(self.pc + 2)
+        self.alu('ADD', operandA, operandB)
+
+    def MUL(self):
+        operandA = self.ram_read(self.pc + 1)
+        operandB = self.ram_read(self.pc + 2)
+        self.alu('MUL', operandA, operandB)
+
+    def PUSH(self):
+        self.SP -= 1
+        address = self.ram[self.pc + 1]
+        value = self.register[address]
+        self.ram[self.SP] = value
+
+    def POP(self):
+        address = self.ram[self.pc + 1]
+        value = self.ram[self.SP]
+        self.register[address] = value
+        self.SP += 1
+
+    def CALL(self):
+        self.SP -= 1
+        ret_address = self.pc + 2
+        self.ram[self.SP] = ret_address
+        reg = self.ram[self.pc + 1]
+        self.pc = self.register[reg]
+
+    def RET(self):
+        ret_address = self.ram[self.SP]
+        self.pc = ret_address
+        self.SP += 1
 
     #STEP 3: Implement 'run' method
     def run(self):
         """Run the CPU."""
-        prog = {
-            'LDI': 0b10000010,
-            'PRN': 0b01000111,
-            'HLT': 0b00000001,
-            'ADD': 0b10100000,
-            'MUL': 0b10100010,
-            'PUSH': 0b01000101,
-            'POP': 0b01000110
-        }
-
+        
         while self.running:
+            IR = self.ram_read(self.pc)
+            if IR in self.branch_table:
+                self.branch_table[IR]()
+                operands = (IR & 0b11000000) >> 6
+                param = (IR & 0b00010000) >> 4
+
+                if not param:
+                    self.pc += operands +1
+
+'''                   
             #IR = _Instruction Register_
             IR = self.ram[self.pc]
             operand_a = self.ram_read(self.pc + 1)
@@ -140,3 +204,4 @@ class CPU:
             else:
                 print(f"Can't find {IR} with index of {self.pc}")
                 sys.exit(1)
+'''
